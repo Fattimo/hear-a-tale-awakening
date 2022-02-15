f54fb01dcc243edd633039f0cdfe436e087b9be4
Fattimo <matt.chen522@gmail.com>
2022-02-15 18:02:46 -0500

    hook continue reading up to a page

--- src/components/Home/ContinueReading.jsx
+++ src/components/Home/ContinueReading.jsx
@@ -1,28 +1,31 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
 
 const ContinueReading = () => {
   return (
     <Box>
       <Text fontWeight={'semibold'}>Continue reading</Text>
-      <Flex
-        mt="3"
-        mx="3"
-        p="4"
-        bgColor="theme.purple"
-        textColor="white"
-        borderRadius="3xl"
-        justify="space-between"
-        align="center"
-        h="50%"
-      >
-        <Box>
-          <Text>Chapter X</Text>
-          <Text>page 14</Text>
-        </Box>
-        <Box>sound</Box>
-        <Box>no sound</Box>
-      </Flex>
+      <Link href="/1" passHref={true}>
+        <Flex
+          mt="3"
+          mx="3"
+          p="4"
+          bgColor="theme.purple"
+          textColor="white"
+          borderRadius="3xl"
+          justify="space-between"
+          align="center"
+          h="50%"
+        >
+          <Box>
+            <Text>Chapter X</Text>
+            <Text>page 14</Text>
+          </Box>
+          <Box>sound</Box>
+          <Box>no sound</Box>
+        </Flex>
+      </Link>
     </Box>
   )
 }
