7c49994a7b6958bab03284f97a4ff82dd3bc1d4f
jlazz890 <72589629+jlazz890@users.noreply.github.com>
2022-02-22 14:27:08 -0500

    Refactor

--- splitter.py
+++ splitter.py
@@ -1,95 +1,80 @@
 import os
 import sys
 import json
-tab = "     "
-paragraphbreak = "\n"
 
-#Usage: py splitter.py pagesize
-pagedir = "public/book/pages/"
-#pagesize: Size of page in characters
-def split(pagesize, linesize):
-	config = {}
-	config["book"] = []
-	config["pages"] = {}
-	#Clean any existing pages
-	for f in os.listdir(pagedir):
-		for f1 in os.listdir(pagedir + str(f)):
-			os.remove(pagedir + str(f) + "/" + str(f1))
-		os.rmdir(pagedir + f)
-	globalpagenum = 1 #Book page number
-	for f in range(1, 52):#Generate chapter
-		chapternum = f
-		os.mkdir(pagedir + str(chapternum))
-		pagenum = 1 #Chapter page number
-		booktxt = None
-		chaptername = None
-		with open("rawchapters/Chapter" + str(f), 'r', encoding='utf8') as book:
-			chaptername = book.readline().strip()
-			booktxt = book.read().replace('\x0c', '\n')#Read book and remove page breaks
-		pos = 0#Character position in text of chapter
-		while pos + pagesize < len(booktxt):
-			end = pagesize
-			
-			while booktxt[pos + end] != " ":#Page must start in between words
-				end -= 1
-			with open(pagedir + str(chapternum) + "/" + str(pagenum) + ".txt", 'w') as page:#Save a page
-				txt = ""
-				if pagenum == 1:
-					txt = tab
-				if booktxt[pos] == " ":
-					txt += booktxt[pos:pos + end].replace("\n", paragraphbreak + tab) #Remove leading space
-				else:
-					txt += booktxt[pos:pos + end].replace("\n", paragraphbreak + tab) #No leading space
 
-				#Split into lines
-				linepos = 0
-				counter = 0
-				while linepos < len(txt):
-					if counter >= linesize and txt[linepos] == " ":
-						txt = txt[:linepos] + "\n" + txt[linepos + 1:]
+class Page_Generator():
+	def __init__(self) -> None:
+		self.tab = "     "
+		self.paragraphbreak = "\n"
+		self.linebreak = "\n"
+		self.pagedir = "public/book/pages/"
+		self.chapternum = 0
+
+	def genpage(self, pagenum, rawtext, linesize):
+		with open(self.pagedir + str(self.chapternum) + "/" + str(pagenum) + ".txt", 'w') as page:#Save a page
+			txt = ""
+			if pagenum == 1:
+				txt = self.tab
+			if rawtext[0] == " ":
+				txt += rawtext.replace("\n", self.paragraphbreak + self.tab) #Remove leading space
+			else:
+				txt += rawtext.replace("\n", self.paragraphbreak + self.tab) #No leading space
+			#Split into lines
+			linepos = 0
+			counter = 0
+			while linepos < len(txt):
+				if counter >= linesize and txt[linepos] == " ":
+					txt = txt[:linepos] + self.linebreak + txt[linepos + 1:]
+				if txt[linepos] == self.linebreak or txt[linepos] == self.paragraphbreak:
+					counter = 0
+				linepos += 1
+				counter += 1
+			page.write(txt)
+	
+	#Usage: py splitter.py pagesize
+	#pagesize: Size of page in characters
+	def split(self, pagesize, linesize):
+		config = {}
+		config["book"] = []
+		config["pages"] = {}
+		#Clean any existing pages
+		for f in os.listdir(self.pagedir):
+			for f1 in os.listdir(self.pagedir + str(f)):
+				os.remove(self.pagedir + str(f) + "/" + str(f1))
+			os.rmdir(self.pagedir + f)
+		globalpagenum = 1 #Book page number
+		for f in range(1, 52):#Generate chapter
+			self.chapternum = f
+			os.mkdir(self.pagedir + str(self.chapternum))
+			pagenum = 1 #Chapter page number
+			booktxt = None
+			chaptername = None
+			with open("rawchapters/Chapter" + str(f), 'r', encoding='utf8') as book:
+				chaptername = book.readline().strip()
+				booktxt = book.read().replace('\x0c', '\n')#Read book and remove page breaks
+			pos = 0#Character position in text of chapter
+			while pos + pagesize < len(booktxt):
+				end = pagesize
+				while booktxt[pos + end] != " ":#Page must start in between words
+					end -= 1
+				self.genpage(pagenum, booktxt[pos:pos + end], linesize)
+				config["pages"][str(globalpagenum)] = {"page":pagenum, "chapter":self.chapternum}
+				pagenum += 1
+				globalpagenum += 1
+				pos = pos + end
+
+			if pos != len(booktxt) - 1:#Save last page
+				self.genpage(pagenum, booktxt[pos:pos + end], linesize)
 					
-					if txt[linepos] == "\n":
-						counter = 0
-					linepos += 1
-					counter += 1
+				config["pages"][str(globalpagenum)] = {"page":pagenum, "chapter":self.chapternum}
+				globalpagenum += 1
+			config["book"].append({"chapter":self.chapternum, "title":chaptername, "pages":pagenum})
 
-
-				page.write(txt)
-			config["pages"][str(globalpagenum)] = {"page":pagenum, "chapter":chapternum}
-			pagenum += 1
-			globalpagenum += 1
-			pos = pos + end
-
-		if pos != len(booktxt) - 1:#Save last page
-			with open(pagedir + str(chapternum) + "/" + str(pagenum) + ".txt", 'w') as page:
-				txt = ""
-				if pagenum == 1:
-					txt = tab
-				if booktxt[pos] == " ":
-					txt += booktxt[pos:pos + end].replace("\n", paragraphbreak + tab) #Remove leading space
-				else:
-					txt += booktxt[pos:pos + end].replace("\n", paragraphbreak + tab) #No leading space
-
-				#Split into lines
-				linepos = 0
-				counter = 0
-				while linepos < len(txt):
-					if counter >= linesize and txt[linepos] == " ":
-						txt = txt[:linepos] + "\n" + txt[linepos + 1:]
-					
-					if txt[linepos] == "\n":
-						counter = 0
-					linepos += 1
-					counter += 1
-				page.write(txt)
-				
-			config["pages"][str(globalpagenum)] = {"page":pagenum, "chapter":chapternum}
-			globalpagenum += 1
-		config["book"].append({"chapter":chapternum, "title":chaptername, "pages":pagenum})
-
-	config["totalPages"] = globalpagenum - 1
-	with open("public/book/config.json", 'w') as configf:
-		json.dump(config, configf)
+		config["totalPages"] = globalpagenum - 1
+		with open("public/book/config.json", 'w') as configf:
+			json.dump(config, configf)
 				
 
-split(int(sys.argv[1]), int(sys.argv[2]))
+splitter = Page_Generator()
+splitter.split(int(sys.argv[1]), int(sys.argv[2]))
