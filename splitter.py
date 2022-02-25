f5dc3c6a51e24ae0ab922e15deaf8d3a313fdcdf
jlazz890 <72589629+jlazz890@users.noreply.github.com>
2022-02-24 20:23:06 -0500

    Fill lines

--- splitter.py
+++ splitter.py
@@ -1,6 +1,14 @@
 import os
+from pdb import line_prefix
 import sys
 import json
+
+charsizes = {'a': 71, 'b': 646, 'c': 83, 'd': 64, 'e': 70, 'f': 118, 'g': 46,
+	'h': 66, 'i': 162, 'j': 159, 'k': 75, 'l': 127, 'm': 44, 'n':66, 'o': 67, 'p':64,
+	'q': 64, 'r': 105, 's': 78, 't': 111, 'u': 67, 'v': 70, 'w': 43, 'x': 71, 'y': 70, 
+	'z': 81, 'A': 51, 'B': 55, 'C':57, 'D': 50, 'E': 64, 'F':68, 'G': 51, 'H': 49,
+	'I': 146, 'J': 123, 'K': 60, 'L': 69, 'M': 44, 'N': 50, 'O': 49, 'P': 59, 'Q': 49,
+	'R': 56, 'S': 61, 'T': 61, 'U': 51, 'V': 54, 'W': 34, 'X': 57, 'Y': 63, 'Z': 63}
 
 #Usage: py splitter.py pagesize linesize
 class Config():
@@ -32,7 +40,7 @@ class Page_Generator():
 		self.chapternum = 0
 		self.cf = Config()
 
-	def genpage(self, pagenum, rawtext, linesize):
+	def genpage(self, pagenum, rawtext):
 		with open(self.pagedir + str(self.chapternum) + "/" + str(pagenum) + ".txt", 'w') as page:#Save a page
 			txt = ""
 			if pagenum == 1:
@@ -43,19 +51,23 @@ class Page_Generator():
 				txt += rawtext.replace("\n", self.paragraphbreak + self.tab) #No leading space
 			#Split into lines
 			linepos = 0
-			counter = 0
+			sumline = 0
 			while linepos < len(txt):
-				if counter >= linesize and txt[linepos] == " ":
-					txt = txt[:linepos] + self.linebreak + txt[linepos + 1:]
-				if txt[linepos:linepos + len(self.linebreak)] == self.linebreak:
-					counter = 0
-					linepos += len(self.linebreak)
-				elif txt[linepos:linepos + len(self.paragraphbreak)] == self.paragraphbreak:
-					counter = 0
-					linepos += len(self.paragraphbreak)
+				if txt[linepos] in charsizes:
+					sumline += 1 / charsizes[txt[linepos]]
 				else:
-					linepos += 1
-				counter += 1
+					sumline += 1/34 #Upper bound for now
+				if txt[linepos] == "\n":
+					sumline = 0
+				
+				if sumline >= 1:				
+					while txt[linepos] != " ":
+						linepos -= 1
+				if sumline >= 1:
+					txt = txt[:linepos] + self.linebreak + txt[linepos + 1:]
+					sumline = 0
+				linepos += 1
+				
 			page.write(txt)
 	
 	#pagesize: Size of page in characters
@@ -80,14 +92,14 @@ class Page_Generator():
 				end = pagesize
 				while booktxt[pos + end] != " ":#Page must start in between words
 					end -= 1
-				self.genpage(pagenum, booktxt[pos:pos + end], linesize)
+				self.genpage(pagenum, booktxt[pos:pos + end])
 				self.cf.addpage(str(globalpagenum), pagenum, self.chapternum)
 				pagenum += 1
 				globalpagenum += 1
 				pos = pos + end
 
 			if pos != len(booktxt) - 1:#Save last page
-				self.genpage(pagenum, booktxt[pos:pos + end], linesize)
+				self.genpage(pagenum, booktxt[pos:pos + end])
 				self.cf.addpage(str(globalpagenum), pagenum, self.chapternum)
 				globalpagenum += 1
 			self.cf.addchapter(chaptername, self.chapternum, pagenum)
