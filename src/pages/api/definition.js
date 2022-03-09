6e76f540095cf1c7ba068631d09b2c01faeddf68
Fattimo <matt.chen522@gmail.com>
2022-03-09 03:01:51 -0500

    api done

--- src/pages/api/definition.js
+++ src/pages/api/definition.js
@@ -1,1 +1,20 @@
-// https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.js
+import clientPromise from 'utils/mongodb'
+
+/**
+ * GET:
+ * word: word to get definition of
+ * res: string containing definition of word
+ */
+const handler = async (req, res) => {
+  const { word } = req.query
+  if (!word) res.status(400).send('Error. No Word Provided')
+  const client = await clientPromise
+  const definitions = client.db('awakening').collection('definitions')
+  const document = await definitions.findOne({
+    words: word,
+  })
+  if (!document) res.status(400).send('Word Definition Not Found.')
+  res.send(document.definition)
+}
+
+export default handler
---
+++ src/pages/api/quiz.js
@@ -0,0 +1,39 @@
+import clientPromise from 'utils/mongodb'
+
+/**
+ * GET params:
+ * letter: letter to get quiz for
+ * res: {
+ *  choices: []
+ *  correctIndex: number
+ *  definition: string
+ * }
+ */
+const handler = async (req, res) => {
+  const { letter } = req.query
+  const QUIZ_SIZE = 4
+  if (!letter) res.status(400).send('Error. No Letter Provided')
+  const client = await clientPromise
+  const definitions = client.db('awakening').collection('definitions')
+  const agg = definitions.aggregate([
+    { $match: { first_letter: letter } },
+    { $sample: { size: QUIZ_SIZE } },
+  ])
+  const payload = {
+    choices: [],
+    correctIndex: Math.floor(Math.random() * QUIZ_SIZE),
+  }
+  let i = 0
+  for await (const document of agg) {
+    if (i === payload.correctIndex) payload.definition = document.definition
+    const choice =
+      document.words[Math.floor(Math.random() * document.words.length)]
+    payload.choices.push(choice)
+    i++
+  }
+  if (payload.choices.length < QUIZ_SIZE)
+    res.status(400).send('Quiz too small.')
+  res.json(payload)
+}
+
+export default handler
