0071efbe37c597d033da950372d42129c68d81fa
jlazz890 <72589629+jlazz890@users.noreply.github.com>
2022-03-11 14:58:12 -0500

    Init

---
+++ definitions.py
@@ -0,0 +1,51 @@
+import pymongo
+import sys
+testDB = "mongodb://localhost:27017/"
+prodDB = ""
+DB = testDB
+if len(sys.argv) >= 2:
+    for i in sys.argv:
+        if i == "-prod":
+            DB = prodDB
+
+client = pymongo.MongoClient(DB)
+db = client["test"]
+col = db["test2"]
+col.delete_many({})#Clear database
+docs = []
+with open("rawdefinitions/names.txt", 'r') as names: #Name definitions
+    for i in names.read().split(","):
+        i = i.strip().replace(" ", "")
+        docs.append({"words": [i.lower()], "definition": "= A name", "first_letter": i.lower()[0]})
+
+with open("rawdefinitions/definitions1.txt", 'r') as defs:#Words
+    lines = defs.readlines()
+    doc = {}
+    s = set()
+    for line in lines:
+        l = line.strip()
+        if l == "":
+            continue
+        if '=' in l:
+            s = set()
+            if doc != {}:
+                docs.append(doc)
+            doc = {"words": [], "definition": "", "first_letter": ""}
+            pos = l.index('=')
+            word = l[:pos-1].lower()
+            if word not in s:
+                doc["words"].append(word)
+                s.add(word)
+            doc["definition"] = l[pos:]
+            doc["first_letter"] = l[0].lower()
+        else:
+            word = l.lower()
+            if word not in s:
+                doc["words"].append(word)
+                s.add(word)
+    docs.append(doc)
+
+        
+col.insert_many(docs)
+col.create_index([("first_letter", pymongo.DESCENDING)])
+col.create_index([("words", pymongo.DESCENDING)])
