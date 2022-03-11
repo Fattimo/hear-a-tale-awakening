de1f54fe9a3430d61697196ef0fb33e7b73de2ed
jlazz890 <72589629+jlazz890@users.noreply.github.com>
2022-03-11 15:50:21 -0500

    Add production DB

--- definitions.py
+++ definitions.py
@@ -1,16 +1,25 @@
 import pymongo
 import sys
 testDB = "mongodb://localhost:27017/"
-prodDB = ""
 DB = testDB
+client = None
+prod = False
 if len(sys.argv) >= 2:
     for i in sys.argv:
         if i == "-prod":
-            DB = prodDB
-
-client = pymongo.MongoClient(DB)
-db = client["test"]
-col = db["test2"]
+            prod = True
+if prod:
+    print("Production")
+    with open(".env", 'r') as env:
+        lines = env.readlines()
+        for line in lines:
+            if line[:8] == "MONGO_DB":
+                auth = line[9:].strip()
+                client = pymongo.MongoClient(auth)
+else:
+    client = pymongo.MongoClient(DB)
+db = client["awakening"]
+col = db["definitions"]
 col.delete_many({})#Clear database
 docs = []
 with open("rawdefinitions/names.txt", 'r', encoding='utf8') as names: #Name definitions
@@ -49,4 +58,3 @@ with open("rawdefinitions/definitions1.txt", 'r', encoding='utf8') as defs:#Word
 col.insert_many(docs)
 col.create_index([("first_letter", pymongo.ASCENDING)])
 col.create_index([("words", pymongo.ASCENDING)])
-print(col.index_information())
