116ec1f4b0d6a87a2c0117db89f72e83918163a3
Fattimo <matt.chen522@gmail.com>
2022-04-13 11:50:20 -0400

    logo + continue button

--- src/components/Home/ContinueReading.jsx
+++ src/components/Home/ContinueReading.jsx
@@ -7,39 +7,39 @@ const ContinueReading = ({ chapter, page }) => {
   return (
     <Box>
       <Text fontWeight={'semibold'}>Continue reading</Text>
-      <NextLink href={`/page/${page}`} passHref={true}>
-        <Link>
-          <Flex
-            mt="3"
-            mx="3"
-            p="4"
-            bgColor="theme.purple"
-            textColor="white"
-            borderRadius="3xl"
-            justify="space-between"
-            align="center"
-            h="50%"
-          >
-            <Box>
-              <Text>Chapter {chapter}</Text>
-              <Text>page {page}</Text>
-            </Box>
-            <Flex>
-              <NextLink href={`/page/${page}?play`} passHref>
-                <Link>
-                  <Flex
-                    bgColor={'white'}
-                    color={'black'}
-                    w={12}
-                    h={12}
-                    borderRadius={'50%'}
-                    align={'center'}
-                    justify={'center'}
-                  >
-                    <HeadphonesIcon />
-                  </Flex>
-                </Link>
-              </NextLink>
+      <Flex
+        mt="3"
+        mx="3"
+        p="4"
+        bgColor="theme.purple"
+        textColor="white"
+        borderRadius="3xl"
+        justify="space-between"
+        align="center"
+        h="50%"
+      >
+        <Box>
+          <Text>Chapter {chapter}</Text>
+          <Text>page {page}</Text>
+        </Box>
+        <Flex>
+          <NextLink href={`/page/${page}?play`} passHref>
+            <Link>
+              <Flex
+                bgColor={'white'}
+                color={'black'}
+                w={12}
+                h={12}
+                borderRadius={'50%'}
+                align={'center'}
+                justify={'center'}
+              >
+                <HeadphonesIcon />
+              </Flex>
+            </Link>
+          </NextLink>
+          <NextLink href={`/page/${page}`} passHref={true}>
+            <Link>
               <Flex
                 bgColor={'white'}
                 color={'black'}
@@ -53,10 +53,10 @@ const ContinueReading = ({ chapter, page }) => {
                 <HeadphonesIcon />
                 <NoIcon position={'absolute'} w={8} h={8} />
               </Flex>
-            </Flex>
-          </Flex>
-        </Link>
-      </NextLink>
+            </Link>
+          </NextLink>
+        </Flex>
+      </Flex>
     </Box>
   )
 }
