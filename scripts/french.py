dec3a2205055d13be45546995f880fb73f110230
jlazz890 <72589629+jlazz890@users.noreply.github.com>
2022-04-15 15:19:12 -0400

    Fix nested phrases issue

--- scripts/french.py
+++ scripts/french.py
@@ -8,6 +8,7 @@ def findandformatfrench(phrases):
     for phrase in phrases:
         if " " in phrase.strip():
             frenchphrases.append(phrase.strip())
+    frenchphrases.sort(key = lambda phrase: len(phrase), reverse=True)
 
     for i in range(1, 40):
         dir = f"../public/book/pages/{i}/"
@@ -17,18 +18,17 @@ def findandformatfrench(phrases):
             with open(dir + str(file), 'r', encoding='utf8') as f:
                 text = f.read()#Read file
                 ltext = text.lower()
-                for phrase in frenchphrases:
+                for i in range(len(frenchphrases)):
                     offset = 0
-                    positions = [f.start() for f in re.finditer(phrase, ltext)]#Get starting positions
+                    positions = [f.start() for f in re.finditer(frenchphrases[i], ltext)]#Get starting positions
                     for pos in positions:
                         #Phrases are all lowercase, so the text must also be lowercase to match them
                         #Get the copy from the original text and mutate that
-                        text = (text[:pos + offset] + text[pos+offset:pos+len(phrase)+offset].replace(" ", "&fr") + 
-                            text[pos+len(phrase) + offset:])
-                        ltext = ltext[:pos + offset] + phrase.replace(" ", "&fr") + ltext[pos+len(phrase) + offset:]
+                        text = (text[:pos + offset] + text[pos+offset:pos+len(frenchphrases[i])+offset].replace(" ", "&fr") + 
+                            text[pos+len(frenchphrases[i]) + offset:])
+                        ltext = ltext[:pos + offset] + frenchphrases[i].replace(" ", "&fr") + ltext[pos+len(frenchphrases[i]) + offset:]
                         offset += 2#File following previous start is now indices behind where it was
 
             with open(dir + str(file), 'w', encoding='utf8') as f:
                 f.write(text)#Write changes
-
     
