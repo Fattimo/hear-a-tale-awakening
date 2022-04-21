843da9b0c065b0f023805b0b1445ca236bb6c1b9
Fattimo <matt.chen522@gmail.com>
2022-04-20 21:36:37 -0400

    nextauth implemented

---
+++ src/pages/api/auth/signup.js
@@ -0,0 +1,39 @@
+import { hash } from 'bcrypt'
+import clientPromise from 'utils/mongodb'
+async function handler(req, res) {
+  //Only POST mothod is accepted
+  if (req.method === 'POST') {
+    //Getting email and password from body
+    const { email, password } = req.body
+    //Validate
+    // if (!email || !email.includes('@') || !password) {
+    //   res.status(422).json({ message: 'Invalid Data' })
+    //   return
+    // }
+    //Connect with database
+    const client = await clientPromise
+    const db = client.db('awakening')
+    //Check existing
+    const checkExisting = await db.collection('users').findOne({ email: email })
+    //Send error response if duplicate user is found
+    if (checkExisting) {
+      res.status(422).json({ message: 'User already exists' })
+      client.close()
+      return
+    }
+    //Hash password
+    const status = await db.collection('users').insertOne({
+      email,
+      password: await hash(password, 12),
+    })
+    //Send success response
+    res.status(201).json({ message: 'User created', ...status })
+    //Close DB connection
+    client.close()
+  } else {
+    //Response for other than POST method
+    res.status(500).json({ message: 'Route not valid' })
+  }
+}
+
+export default handler
